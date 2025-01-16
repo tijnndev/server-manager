753dd9abe76cde56f489e8d81d1084f4eb3d7a9d
Tijn <99891906+tijnndev@users.noreply.github.com>
2025-01-16 12:16:43 +0100

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -2,7 +2,6 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 cd /etc/server-manager
 /usr/bin/git stash
-/usr/bin/git status
 /usr/bin/git pull -v
 # pip install -r requirements.txt
 # alembic upgrade --autogenerate head
