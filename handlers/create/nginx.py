79ca14261fd92fb91f9371297be871d63d6a6640
Tijn <99891906+tijnndev@users.noreply.github.com>
2025-03-13 00:54:05 +0100

    Update nginx.py

--- handlers/create/nginx.py
+++ handlers/create/nginx.py
@@ -7,6 +7,7 @@ def create_docker_file(process, dockerfile_path):
         dockerfile_content = """FROM nginx:alpine
 WORKDIR /usr/share/nginx/html
 COPY . /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/nginx.conf
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
 """
